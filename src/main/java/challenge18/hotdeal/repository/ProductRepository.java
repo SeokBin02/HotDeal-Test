6c263e324cf731763b653c30ca453f1e43f2a447
Joyanggi <jyg8033@gmail.com>
2023-05-23 20:24:57 +0900

    ➖Remove: JmeterTest 삭제

--- src/main/java/challenge18/hotdeal/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package challenge18.hotdeal.repository;
-
-import challenge18.hotdeal.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
