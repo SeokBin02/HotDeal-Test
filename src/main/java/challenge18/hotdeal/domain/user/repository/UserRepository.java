4bc933f244c719dbf545a9c55d53be5f52d160f4
Kim Seonmin <w.hand715@gmail.com>
2023-05-26 21:35:55 +0900

    🔨Refactor: UserRepository 내부 불필요한 @Transactional 제거

--- src/main/java/challenge18/hotdeal/domain/user/repository/UserRepository.java
+++ src/main/java/challenge18/hotdeal/domain/user/repository/UserRepository.java
@@ -5,15 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByUserId(String userId);
 
-    @Transactional
-    @Modifying(clearAutomatically = true)
+    @Modifying
     @Query(value = "insert into users (user_id, password, role) VALUES (:userId, :password, :role)", nativeQuery = true)
     void insert(@Param("userId") String userId, @Param("password") String password, @Param("role") String role);
 }
